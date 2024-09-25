040a839d10bb2587f7647dae13e7d1192309ca2e
Nikhil R <nikhil2002raghu@gmail.com>
2024-09-25 16:36:46 +0530

    weatherapp

--- src/weatherapp/slice/WeatherApi.ts
+++ src/weatherapp/slice/WeatherApi.ts
@@ -1,13 +1,14 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const apiKey = 'ffe6045af019031036d19edcb02e9781';
+
 const apiKey = import.meta.env.VITE_API_KEY;
+const baseUrl = import.meta.env.VITE_BASE_URL;
 console.log(apiKey);
 
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: baseUrl}),
     endpoints: (builder) => ({
         getWeather: builder.query<any, string>({
             query: (district) => `weather?q=${district}&appid=${apiKey}&units=metric`,
