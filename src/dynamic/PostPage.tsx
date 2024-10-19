6647026ac12d54380c828126ed4e77ab0c1eef67
Nikhil R <nikhil2002raghu@gmail.com>
2024-10-19 14:21:53 +0530

    updated folders

--- src/dynamic/PostPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const PostPage: React.FC = () => {
-    const { id } = useParams<{ id: string }>();
-  
-    return <h1>Displaying Post with ID: {id}</h1>;
-  };
-  export default PostPage;
--- src/nested/Pages.tsx
+++
@@ -1,3 +0,0 @@
-export const Overview: React.FC = () => <h2>Dashboard Overview</h2>;
-export const Settings: React.FC = () => <h2>Dashboard Settings</h2>;
-export const Profile: React.FC = () => <h2>Dashboard Profile</h2>;
